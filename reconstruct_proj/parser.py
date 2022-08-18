fd55a33f40615229b8f2e49e6556dbdfbf0bdd57
Begelit <dmilevch@gmail.com>
2022-08-18 18:35:40 +0300

    parser

--- reconstruct_proj/parser.py
+++ reconstruct_proj/parser.py
@@ -33,6 +33,7 @@ def get_product_info(driver):
 		product_detail_info_elem = WebDriverWait(driver, 30).until(EC.presence_of_element_located(('xpath', '//div[@class="product-detail-info"]')))
 		color_selector_element_class_name = 'product-detail-color-selector product-detail-info__color-selector'
 		product_detail_info_elem_innerHTML = product_detail_info_elem.get_attribute('innerHTML')
+		productInfo_dict = dict()
 		
 		if color_selector_element_class_name in product_detail_info_elem_innerHTML:
 		
@@ -41,8 +42,6 @@ def get_product_info(driver):
 			
 			UL_element_color = WebDriverWait(driver, 30).until(EC.presence_of_element_located(('xpath', colors_list_path)))
 			LI_elements_color = UL_element_color.find_elements(By.XPATH,'//li[@class="product-detail-color-selector__color"]')
-
-			productInfo_dict = dict()
 
 			for i,li in enumerate(LI_elements_color):
 				
@@ -63,9 +62,18 @@ def get_product_info(driver):
 				UL_element_size = WebDriverWait(driver, 30).until(EC.presence_of_element_located(('xpath', sizes_list_path)))
 				LI_elements_size = UL_element_size.find_elements(By.XPATH,'//li[@class = "product-detail-size-selector__size-list-item"]')
 				productInfo_dict[color]['size'] = [driver.find_element(By.XPATH,sizes_list_path+'//li[@id = "{id_li}"]//span[@class = "product-detail-size-info__main-label"]'.format(id_li = li.get_attribute('id'))).text for li in LI_elements_size]
+			#print(productInfo_dict)
 			return productInfo_dict
 		else:
+			sizes_list_path = '//div[@class="product-detail-size-selector__size-list-wrapper product-detail-size-selector__size-list-wrapper--open"]//ul[@class="product-detail-size-selector__size-list"]'
 			
+			color = WebDriverWait(driver, 30).until(EC.visibility_of_element_located((By.XPATH,'//p[@class="product-detail-selected-color product-detail-info__color"]')))
+			color = color.text.split('|')[0].replace(' ','')
+			
+			productInfo_dict[color] = dict()
+			
+			productInfo_dict[color]['price'] = WebDriverWait(driver, 30).until(EC.visibility_of_element_located((By.XPATH,'//div[@class="product-detail-info__price"]//div[@class="money-amount price-formatted__price-amount"]//span[@class="money-amount__main"]'))).text
+			print(productInfo_dict)
 	except Exception as e:
 		print(traceback.format_exc())
 		driver.close()
@@ -75,8 +83,8 @@ def get_product_info(driver):
 if __name__ == "__main__":
 	print('!!!!!!!!!!!!!!!!!!!!!!!!!')
 	driver_path = '/home/koza/Reps/HEIN_FROMgit/shein_bot/drivers/chromedriver'
-	url = 'https://www.zara.com/ru/ru/%D0%BE%D0%B1%D1%8A%D0%B5%D0%BC%D0%BD%D1%8B%D0%B8-%D0%B4%D0%B2%D1%83%D0%B1%D0%BE%D1%80%D1%82%D0%BD%D1%8B%D0%B8-%D0%BF%D0%B8%D0%B4%D0%B6%D0%B0%D0%BA-p02753032.html?v1=206087228'
-	#url = 'https://www.zara.com/ru/ru/%D0%BF%D0%B8%D0%B4%D0%B6%D0%B0%D0%BA-%D0%B8%D0%B7-%D1%81%D1%82%D1%80%D1%83%D1%8F%D1%89%D0%B5%D0%B8%D1%81%D1%8F-%D1%82%D0%BA%D0%B0%D0%BD%D0%B8-p01255709.html?v1=179012832'
+	#url = 'https://www.zara.com/ru/ru/%D0%BE%D0%B1%D1%8A%D0%B5%D0%BC%D0%BD%D1%8B%D0%B8-%D0%B4%D0%B2%D1%83%D0%B1%D0%BE%D1%80%D1%82%D0%BD%D1%8B%D0%B8-%D0%BF%D0%B8%D0%B4%D0%B6%D0%B0%D0%BA-p02753032.html?v1=206087228'
+	url = 'https://www.zara.com/ru/ru/%D0%BF%D0%B8%D0%B4%D0%B6%D0%B0%D0%BA-%D0%B8%D0%B7-%D1%81%D1%82%D1%80%D1%83%D1%8F%D1%89%D0%B5%D0%B8%D1%81%D1%8F-%D1%82%D0%BA%D0%B0%D0%BD%D0%B8-p01255709.html?v1=179012832'
 	driver = start_driverSession(driver_path=driver_path)
 	driver_getSource = get_page_source(driver,url)
 	#print(driver_getSource.page_source)
